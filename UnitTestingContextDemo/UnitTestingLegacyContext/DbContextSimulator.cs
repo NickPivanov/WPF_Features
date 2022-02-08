3dc5240a14571ce35693c2301a3adf4a98c73111
Pivanov Nikolay <it3@tstarter.ru>
2022-02-08 10:34:05 +0200

    unit testing legacy code

---
+++ UnitTestingContextDemo/UnitTestingLegacyContext/DbContextSimulator.cs
@@ -0,0 +1,19 @@
+﻿using UnitTestingLegacyContext.Models;
+
+namespace UnitTestingLegacyContext
+{
+    public class DbContextSimulator
+    {
+        public IList<User> Users { get;set; }
+
+        public DbContextSimulator()
+        {
+            Users = new List<User>()
+            { 
+                new User() { Id = 1, Name = "Obi Van Kenobi"},
+                new User() { Id = 2, Name = "Luke Skywalker"}
+            };
+
+        }
+    }
+}
---
+++ UnitTestingContextDemo/UnitTestingLegacyContext/SomeNewFunctionality/DataOperations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingLegacyContext.Models;
+
+namespace UnitTestingLegacyContext.SomeNewFunctionality
+{
+    public class DataOperations
+    {
+        private readonly ContextService _contextService;
+
+        public DataOperations() : this(new ContextService())
+        {
+
+        }
+
+        public DataOperations(ContextService contextServiceWrapper)
+        {
+            _contextService = contextServiceWrapper;
+        }
+
+        public int AddNewUser(string userName)
+        {
+            return _contextService.AddUser(userName);
+        }
+
+        public User GetUser(int id)
+        {
+            return _contextService.GetUserByID(id);
+        }
+    }
+
+    //this class will be used for unit-testing
+    //methods should be marked as virtual
+    public class ContextService
+    {
+        private DbContextSimulator ctx = new DbContextSimulator();
+        public virtual int AddUser(string name)
+        {
+            var user = new User { Id = ctx.Users.Count + 1, Name = name };
+            ctx.Users.Add(user);
+            return user.Id;
+        }
+
+        public virtual User GetUserByID(int id)
+        {
+            return ctx.Users.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
